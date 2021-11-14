d790d6a8aaa29783c76615dfeab6a8f1549d0810
Yavuz Günay <yavuzgunay@gmail.com>
2021-11-14 22:00:04 +0100

    parameterize Rest Service Address

--- src/rest-call.ts
+++ src/rest-call.ts
@@ -1,15 +1,31 @@
 import * as SDK from 'azure-devops-extension-sdk';
 import axios from 'axios';
+import { IWorkItemFormService, WorkItemTrackingServiceIds } from 'azure-devops-extension-api/WorkItemTracking/WorkItemTrackingServices';
 
 export async function LoadDataFromService() {
     const inputs = SDK.getConfiguration().witInputs;
     const address = inputs.RestServiceAddress;
     const username = inputs.RestServiceUserName;
     const password = inputs.RestServicePassword;
+    const service = await SDK.getService<IWorkItemFormService>(WorkItemTrackingServiceIds.WorkItemFormService);
+
     var params: any = {}
     if (inputs.RestCallParameters) {
         try {
             params = JSON.parse(inputs.RestCallParameters);
+
+            // iterate through params to see if any value needs to be replaced with field values
+            for (const key of Object.keys(params)) {
+                const propertyValue = params[key];
+                const isParameterized = propertyValue.startsWith('{') && propertyValue.endsWith('}');
+                if (isParameterized) {
+                    const fieldName = propertyValue.replace('{', '').replace('}', '');
+                    const value = await service.getFieldValue(fieldName);
+
+                    // replace parameter with the field value
+                    params[key] = value;
+                }
+            }
         } catch (err) {
             console.log(err)
             return;
@@ -36,7 +52,7 @@ export async function LoadDataFromService() {
             params: params
         }
     ).catch(err => {
-        console.log(err); 
+        console.log(err);
         throw err;
     });
 }
